8bb6420be6169b679c0d747e2da26bfbe2ab2208
Marcelo Coronel <marcor19256@fpuna.edu.py>
2022-05-27 17:12:47 -0400

    Agregado de estructura principal

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import { ingresoDatos, opciones } from './funciones.js';
-
+import { menu, opciones } from './funciones.js';
+function validacionConValidacion() {
+    let opcion = menu()
+    while (opcion < 1 || opcion > 3) {
+        if (opcion < 1 || opcion > 3) {
+            console.clear()
+            console.log('Datos ingresados incorrectos, vuelva a ingresar nuevamente!')
+            opcion = menu()
+        }
+    }
+    return opcion
+}
+let condicionFinal = 1
 //Menu
 console.log('Bienvenidos al Juego Generala JS')
-let opcion = ingresoDatos()
-while (opcion < 1 || opcion > 3) {
-    if (opcion < 1 || opcion > 3) {
+while (condicionFinal) {
+    let retorno = opciones(validacionConValidacion())
+    if (retorno == 1) {
+        condicionFinal = (prompt('Desea volver al Menu Principal? Ingrese un valor para continuar o solamente presione Enter para salir'))
         console.clear()
-        console.log('Datos ingresados incorrectos, vuelva a ingresar nuevamente!')
-        opcion = ingresoDatos()
+        if (condicionFinal == undefined) {
+            console.log('Gracias por Jugar !!')
+        }
+    }
+    else {
+        break;
     }
 }
-
