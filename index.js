f5dfb982c2b63bcc1328ccd6bc21968d05b52d7e
Marcelo Coronel <marcor19256@fpuna.edu.py>
2022-05-29 20:24:26 -0400

    La funcion de validacion se extraera del modulo de funciones correspondientes

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-import { menu, opciones } from './funciones.js';
-function validacionConValidacion() {
-    let opcion = menu()
-    while (opcion < 1 || opcion > 3) {
-        if (opcion < 1 || opcion > 3) {
-            console.clear()
-            console.log('Datos ingresados incorrectos, vuelva a ingresar nuevamente!')
-            opcion = menu()
-        }
-    }
-    return opcion
-}
+import { opciones, validacionDeOpciones1 } from './funciones.js';
+
 let condicionFinal = 1
 //Menu
 console.log('Bienvenidos al Juego Generala JS')
 while (condicionFinal) {
-    let retorno = opciones(validacionConValidacion())
+    let retorno = opciones(validacionDeOpciones1())
     if (retorno == 1) {
         condicionFinal = (prompt('Desea volver al Menu Principal? Ingrese un valor para continuar o solamente presione Enter para salir'))
         console.clear()
